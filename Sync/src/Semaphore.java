699f3e29ea16e165cc3331f4c731e9c9b76b856a
mahmoudhosni200 <20230370@stud.fci-cu.edu.eg>
2025-11-04 21:51:46 +0200

    add semaphore

--- Sync/src/Semaphore.java
+++ Sync/src/Semaphore.java
@@ -1,2 +1,22 @@
 public class Semaphore {
+    private int value = 0;
+    public Semaphore(int initial) {
+        value = initial;
+    }
+
+    public synchronized void waitSem() throws InterruptedException {
+        value--;
+        if(value < 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                
+            }
+        }
+        
+    }
+    public synchronized void signalSem() {
+        value++;
+        notify();
+    }
 }

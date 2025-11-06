faac6df57d53c718a731560eb7fdbf64f35b93f9
adham_19 <159729301+adham19999@users.noreply.github.com>
2025-11-06 16:17:39 +0200

    person 3 task done

--- Sync/src/Car.java
+++ Sync/src/Car.java
@@ -1,2 +1,27 @@
-public class Car {
+public class Car extends Thread {
+    private final int id;
+    private final SharedBuffer buffer;
+    public Car(int id, SharedBuffer buffer) {
+        this.id = id;
+        this.buffer = buffer;
+    }
+    @Override
+    public void run() {
+        System.out.println("Car " + id + " arrives");
+        try {
+            buffer.enqueue(this);
+            System.out.println("Car " + id + " enters queue");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Car " + id + " was interrupted");
+        }
+    }
+    public int getIdValue() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Car-" + id;
+    }
 }
